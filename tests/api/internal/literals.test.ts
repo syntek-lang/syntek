1cec75e0d211c0272a512fa155dd46cd6669f6ed
SebastiaanYN <sebastiaanyn@gmail.com>
2019-04-28 15:03:40 +0200

    Add tests for objects

--- tests/api/internal/literals.test.ts
+++ tests/api/internal/literals.test.ts
@@ -1,8 +1,13 @@
+/* eslint-disable prefer-arrow-callback, func-names */
+
 import 'mocha';
 import { expect } from 'chai';
 
 import Syntek from '../../../src/api/internal/Syntek';
-import { DataType, NumberStruct } from '../../../src/api/internal/structures';
+import { Context } from '../../../src/api/internal/handlers';
+import {
+  DataType, NumberStruct, ObjectStruct, FunctionStruct, VariableStruct,
+} from '../../../src/api/internal/structures';
 
 describe('Literals', () => {
   const syntek: Syntek = new Syntek();
@@ -32,6 +37,59 @@ describe('Literals', () => {
     it('throws an error when used as a function', () => {
       expect(positiveNumber.exec).to.throw();
       expect(negativeNumber.exec).to.throw();
+    });
+  });
+
+  describe('object', () => {
+    it('creates an object correctly', () => {
+      const object = syntek.literalHandler.object(new Context(), function () {});
+
+      expect(object).to.be.an.instanceof(ObjectStruct);
+      expect(object.type).to.equal(DataType.OBJECT);
+    });
+
+    it('correctly stores function declarations', () => {
+      const object = syntek.literalHandler.object(new Context(), function () {
+        this.declareFunction('fn', [], function () {
+          return syntek.literalHandler.number(5);
+        }, DataType.NUMBER);
+      });
+
+      const fn = object.getProperty('fn');
+      expect(fn).to.be.an.instanceof(FunctionStruct);
+      expect(fn.type).to.equal(DataType.FUNCTION);
+
+      const returnValue: NumberStruct = fn.exec(new Context(), []);
+      expect(returnValue).to.be.an.instanceof(NumberStruct);
+      expect(returnValue.toNumber()).to.equal(5);
+    });
+
+    it('correctly stores variable declarations', () => {
+      const object = syntek.literalHandler.object(new Context(), function () {
+        this.declareVariable('x', syntek.literalHandler.number(5));
+      });
+
+      const x = object.getProperty('x');
+      expect(x).to.be.an.instanceof(VariableStruct);
+      expect(x.type).to.equal(DataType.NUMBER);
+      expect(x.toNumber()).to.equal(5);
+    });
+
+    it('correctly stores object declarations', () => {
+      const object = syntek.literalHandler.object(new Context(), function () {
+        this.declareVariable('nested', syntek.literalHandler.object(this, function () {
+          this.declareVariable('x', syntek.literalHandler.number(5));
+        }));
+      });
+
+      const nested = object.getProperty('nested');
+      expect(nested).to.be.an.instanceof(VariableStruct);
+      expect(nested.type).to.equal(DataType.OBJECT);
+
+      const x = nested.getProperty('x');
+      expect(x).to.be.an.instanceof(VariableStruct);
+      expect(x.type).to.equal(DataType.NUMBER);
+      expect(x.toNumber()).to.equal(5);
     });
   });
 });
