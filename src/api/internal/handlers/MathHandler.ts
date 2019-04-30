ff793f3d4ff753dff7f02a202c8e5f6bad587868
SebastiaanYN <sebastiaanyn@gmail.com>
2019-04-30 22:43:06 +0200

    Add remaining math methods

--- src/api/internal/handlers/MathHandler.ts
+++ src/api/internal/handlers/MathHandler.ts
@@ -2,13 +2,68 @@ import { Struct, NumberStruct } from '../structures';
 
 export default class MathHandler {
   /**
-   * Add 2 numbers together
+   * Add 2 numbers
    *
    * @param a - The left hand side
    * @param b - The right hand side
-   * @returns A number structure containing the sum of a and b
+   * @returns A number structure containing the outcome of `a+b`
    */
   static add(a: Struct, b: Struct): NumberStruct {
     return new NumberStruct(a.toNumber() + b.toNumber());
+  }
+
+  /**
+   * Subtracts 2 numbers
+   *
+   * @param a - The left hand side
+   * @param b - The right hand side
+   * @returns A number structure containing the outcome of `a-b`
+   */
+  static subtract(a: Struct, b: Struct): NumberStruct {
+    return new NumberStruct(a.toNumber() - b.toNumber());
+  }
+
+  /**
+   * Multiply 2 numbers
+   *
+   * @param a - The left hand side
+   * @param b - The right hand side
+   * @returns A number structure containing the outcome of `a*b`
+   */
+  static multiply(a: Struct, b: Struct): NumberStruct {
+    return new NumberStruct(a.toNumber() * b.toNumber());
+  }
+
+  /**
+   * Divide 2 numbers
+   *
+   * @param a - The left hand side
+   * @param b - The right hand side
+   * @returns A number structure containing the outcome of `a/b`
+   */
+  static divide(a: Struct, b: Struct): NumberStruct {
+    return new NumberStruct(a.toNumber() / b.toNumber());
+  }
+
+  /**
+   * Modulo 2 numbers
+   *
+   * @param a - The left hand side
+   * @param b - The right hand side
+   * @returns A number structure containing the outcome of `a%b`
+   */
+  static modulo(a: Struct, b: Struct): NumberStruct {
+    return new NumberStruct(a.toNumber() % b.toNumber());
+  }
+
+  /**
+   * Pow 2 numbers
+   *
+   * @param a - The left hand side
+   * @param b - The right hand side
+   * @returns A number structure containing the outcome of `a^b`
+   */
+  static pow(a: Struct, b: Struct): NumberStruct {
+    return new NumberStruct(a.toNumber() ** b.toNumber());
   }
 }
--- tests/api/internal/math.test.ts
+++ tests/api/internal/math.test.ts
@@ -52,4 +52,234 @@ describe('Math', () => {
       expect(number.toNumber()).to.equal(-5);
     });
   });
+
+  describe('subtract', () => {
+    it('correctly subtracts 2 positive numbers', () => {
+      const number = syntek.mathHandler.subtract(
+        syntek.literalHandler.number(10),
+        syntek.literalHandler.number(5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(5);
+    });
+
+    it('correctly subtracts 2 negative numbers', () => {
+      const number = syntek.mathHandler.subtract(
+        syntek.literalHandler.number(-10),
+        syntek.literalHandler.number(-5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(-5);
+    });
+
+    it('correctly subtracts a positive and negative number', () => {
+      const number = syntek.mathHandler.subtract(
+        syntek.literalHandler.number(10),
+        syntek.literalHandler.number(-5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(15);
+    });
+
+    it('correctly subtracts a negative and positive number', () => {
+      const number = syntek.mathHandler.subtract(
+        syntek.literalHandler.number(-10),
+        syntek.literalHandler.number(5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(-15);
+    });
+  });
+
+  describe('multiply', () => {
+    it('correctly multiplies 2 positive numbers', () => {
+      const number = syntek.mathHandler.multiply(
+        syntek.literalHandler.number(10),
+        syntek.literalHandler.number(5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(50);
+    });
+
+    it('correctly multiplies 2 negative numbers', () => {
+      const number = syntek.mathHandler.multiply(
+        syntek.literalHandler.number(-10),
+        syntek.literalHandler.number(-5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(50);
+    });
+
+    it('correctly multiplies a positive and negative number', () => {
+      const number = syntek.mathHandler.multiply(
+        syntek.literalHandler.number(10),
+        syntek.literalHandler.number(-5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(-50);
+    });
+
+    it('correctly multiplies a negative and positive number', () => {
+      const number = syntek.mathHandler.multiply(
+        syntek.literalHandler.number(-10),
+        syntek.literalHandler.number(5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(-50);
+    });
+  });
+
+  describe('divide', () => {
+    it('correctly divides 2 positive numbers', () => {
+      const number = syntek.mathHandler.divide(
+        syntek.literalHandler.number(10),
+        syntek.literalHandler.number(5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(2);
+    });
+
+    it('correctly divides 2 negative numbers', () => {
+      const number = syntek.mathHandler.divide(
+        syntek.literalHandler.number(-10),
+        syntek.literalHandler.number(-5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(2);
+    });
+
+    it('correctly divides a positive and negative number', () => {
+      const number = syntek.mathHandler.divide(
+        syntek.literalHandler.number(10),
+        syntek.literalHandler.number(-5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(-2);
+    });
+
+    it('correctly divides a negative and positive number', () => {
+      const number = syntek.mathHandler.divide(
+        syntek.literalHandler.number(-10),
+        syntek.literalHandler.number(5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(-2);
+    });
+  });
+
+  describe('modulo', () => {
+    it('correctly returns the module of 2 positive numbers', () => {
+      const number = syntek.mathHandler.modulo(
+        syntek.literalHandler.number(12),
+        syntek.literalHandler.number(5),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(2);
+    });
+
+    it('correctly returns the module of 2 negative numbers', () => {
+      const number = syntek.mathHandler.modulo(
+        syntek.literalHandler.number(-6),
+        syntek.literalHandler.number(-4),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(-2);
+    });
+
+    it('correctly returns the module of a positive and negative number', () => {
+      const number = syntek.mathHandler.modulo(
+        syntek.literalHandler.number(11),
+        syntek.literalHandler.number(-6),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(5);
+    });
+
+    it('correctly returns the module of a negative and positive number', () => {
+      const number = syntek.mathHandler.modulo(
+        syntek.literalHandler.number(-10),
+        syntek.literalHandler.number(6),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(-4);
+    });
+  });
+
+  describe('pow', () => {
+    it('correctly returns the pow of 2 positive numbers', () => {
+      const number = syntek.mathHandler.pow(
+        syntek.literalHandler.number(2),
+        syntek.literalHandler.number(4),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(16);
+    });
+
+    it('correctly returns the pow of 2 negative numbers', () => {
+      const number = syntek.mathHandler.pow(
+        syntek.literalHandler.number(-4),
+        syntek.literalHandler.number(-2),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(0.0625);
+    });
+
+    it('correctly returns the pow of a positive and negative number', () => {
+      const number = syntek.mathHandler.pow(
+        syntek.literalHandler.number(2),
+        syntek.literalHandler.number(-3),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(0.125);
+    });
+
+    it('correctly returns the pow of a negative and positive number', () => {
+      const number = syntek.mathHandler.pow(
+        syntek.literalHandler.number(-5),
+        syntek.literalHandler.number(4),
+      );
+
+      expect(number).to.be.an.instanceof(NumberStruct);
+      expect(number.type).to.equal(DataType.NUMBER);
+      expect(number.toNumber()).to.equal(625);
+    });
+  });
 });
