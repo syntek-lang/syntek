d8b7f130a13870f15c664d1fbc40a4c586b7d583
SebastiaanYN <sebastiaanyn@gmail.com>
2019-06-30 14:19:32 +0200

    Make exponentiation right associative

--- src/compiler/parser/matchers/expressions/op9.ts
+++ src/compiler/parser/matchers/expressions/op9.ts
@@ -11,7 +11,7 @@ export function op9(this: ExpressionMatcher): Node {
   while (this.match(LexicalToken.CARET)) {
     const operator = this.previous();
     this.eatWhitespace();
-    const right = this.op10();
+    const right = this.op9();
 
     expr = new Expressions.BinaryExpression(expr, operator, right, {
       start: expr.location.start,
