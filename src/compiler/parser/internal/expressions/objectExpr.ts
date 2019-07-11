bc06506b75ecfcd29283f5dfc4cc24f502413b1b
SebastiaanYN <sebastiaanyn@gmail.com>
2019-07-11 20:44:21 +0200

    Add object expr tests and fix object expr parsing

--- src/compiler/parser/internal/expressions/objectExpr.ts
+++ src/compiler/parser/internal/expressions/objectExpr.ts
@@ -6,11 +6,20 @@ import { Parser } from '../../..';
 
 export function objectExpr(parser: Parser, prefix: Token): Node {
   const start = prefix.location.start;
-  parser.eatWhitespace();
-
   const props: Node[] = [];
-  while (!parser.match(LexicalToken.RBRACE)) {
-    props.push(parser.declaration());
+
+  if (parser.peekIgnoreWhitespace().type === LexicalToken.RBRACE) {
+    parser.eatWhitespace();
+    parser.advance();
+  } else {
+    parser.consume(LexicalToken.NEWLINE, 'Expected newline after "{"');
+    parser.consume(LexicalToken.INDENT, 'Expected indent after "{"');
+
+    while (!parser.match(LexicalToken.OUTDENT)) {
+      props.push(parser.declaration());
+    }
+
+    parser.consume(LexicalToken.RBRACE, 'Expected after outdent');
   }
 
   return new ObjectExpression(props, {
