3c8ae0b12da0fb9782df8fcca4625f3492a07967
SebastiaanYN <sebastiaanyn@gmail.com>
2019-07-14 16:12:41 +0200

    Add class and variable decl tests

---
+++ tests/compiler/parser/declarations/class/class.test.ts
@@ -0,0 +1,23 @@
+import 'mocha';
+
+describe('class', () => {
+  it('parses single var correctly');
+
+  it('parses multi var correctly');
+
+  it('parses single static var correctly');
+
+  it('parses multi static var correctly');
+
+  it('parses single func correctly');
+
+  it('parses multi func correctly');
+
+  it('parses single static func correctly');
+
+  it('parses multi static func correctly');
+
+  it('parses single extend correctly');
+
+  it('parses multi extend correctly');
+});
---
+++ tests/compiler/parser/declarations/variable/variable.test.ts
@@ -0,0 +1,11 @@
+import 'mocha';
+
+describe('variable', () => {
+  it('parses without type correctly');
+
+  it('parses with type no array depth correctly');
+
+  it('parses with type single array depth correctly');
+
+  it('parses with type multi array depth correctly');
+});
