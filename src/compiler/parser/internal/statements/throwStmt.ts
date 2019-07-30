9600a1e45b9aa0449056aca35100ffe2c85e7c76
SebastiaanYN <sebastiaanyn@gmail.com>
2019-07-30 16:46:38 +0200

    Ignore whitespace after 'throw'

--- src/compiler/parser/internal/statements/throwStmt.ts
+++ src/compiler/parser/internal/statements/throwStmt.ts
@@ -5,14 +5,15 @@ import { Span } from '../../../../position';
 
 export function throwStmt(parser: Parser): Node {
   const throwSpan = parser.previous().span;
+  parser.eatWhitespace();
 
   const expression = parser.expression("Expected an expression after 'throw'", (error) => {
     error.info('Add an expression after this throw', throwSpan);
   });
+
   parser.consume(LexicalToken.NEWLINE, 'Expected a newline after the throw statement', (error) => {
     error.info('Add a newline after this expression', expression.span);
   });
-
   parser.syncIndentation();
 
   return new ThrowStatement(expression, new Span(throwSpan.start, parser.previous().span.end));
