5a0347bc3a44f92c5ef9bb172b5bd7b352ce506d
SebastiaanYN <sebastiaanyn@gmail.com>
2019-09-27 16:21:00 +0200

    Remove repeat and conditional, update precedence and parse-rules

--- tests/parser/tokenizer/operators-punctuation.test.ts
+++ tests/parser/tokenizer/operators-punctuation.test.ts
@@ -13,17 +13,28 @@ const map = {
   '/': LexicalToken.SLASH,
   '%': LexicalToken.PERCENT,
   '^': LexicalToken.CARET,
+
   '=': LexicalToken.EQUAL,
+  '==': LexicalToken.EQUAL_EQUAL,
+
+  '!': LexicalToken.BANG,
+  '!=': LexicalToken.BANG_EQUAL,
+
+  '<': LexicalToken.LT,
+  '<=': LexicalToken.LT_EQUAL,
+
+  '>': LexicalToken.GT,
+  '>=': LexicalToken.GT_EQUAL,
 
   // Punctuation
   '.': LexicalToken.DOT,
   ',': LexicalToken.COMMA,
-  '[': LexicalToken.LSQB,
-  ']': LexicalToken.RSQB,
-  '(': LexicalToken.LPAR,
-  ')': LexicalToken.RPAR,
-  '<': LexicalToken.LT,
-  '>': LexicalToken.GT,
+  '[': LexicalToken.L_SQB,
+  ']': LexicalToken.R_SQB,
+  '(': LexicalToken.L_PAR,
+  ')': LexicalToken.R_PAR,
+  '{': LexicalToken.L_BRACE,
+  '}': LexicalToken.R_BRACE,
   ':': LexicalToken.COLON,
 };
 
@@ -34,7 +45,7 @@ describe('operators and punctuation', () => {
 
       expect(tokens[0].type).to.equal(type);
       expect(tokens[0].lexeme).to.equal(char);
-      expect(tokens[0].span).to.deep.equal({ start: [0, 0], end: [0, 1] });
+      expect(tokens[0].span).to.deep.equal({ start: [0, 0], end: [0, char.length] });
     });
   });
 });
