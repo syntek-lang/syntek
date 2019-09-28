df7adca96d5788b39b706477b4d11d9822b67196
SebastiaanYN <sebastiaanyn@gmail.com>
2019-09-28 18:50:26 +0200

    Fix literal parsing and tests

--- tests/parser/literals/string/string.test.ts
+++ tests/parser/literals/string/string.test.ts
@@ -8,8 +8,23 @@ import { SyntacticToken } from '../../../../src/grammar/SyntacticToken';
 import { ExpressionStatement } from '../../../../src/grammar/nodes/Statements';
 
 describe('string', () => {
-  it('parses correctly', () => {
-    const valid = loadRaw(__dirname, './string.tek');
+  it('parses single quote correctly', () => {
+    const valid = loadRaw(__dirname, './single-quote.tek');
+    const strings = valid.split('\n');
+
+    parse(valid).body.forEach((node, i) => {
+      expect(node.type).to.equal(SyntacticToken.EXPRESSION_STMT);
+      expect(node).to.be.an.instanceof(ExpressionStatement);
+
+      const expr = (node as ExpressionStatement).expression as Literal;
+      expect(expr.type).to.equal(SyntacticToken.LITERAL);
+      expect(expr).to.be.an.instanceof(Literal);
+      expect(expr.value.lexeme).to.equal(strings[i]);
+    });
+  });
+
+  it('parses double quote correctly', () => {
+    const valid = loadRaw(__dirname, './double-quote.tek');
     const strings = valid.split('\n');
 
     parse(valid).body.forEach((node, i) => {
