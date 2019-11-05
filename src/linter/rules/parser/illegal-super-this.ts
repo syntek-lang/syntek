96a237ea518c84dddd8602525b2478ebc7438d16
SebastiaanYN <sebastiaanyn@gmail.com>
2019-11-05 14:40:59 +0100

    Check if super or this is used in a static class property

--- src/linter/rules/parser/illegal-super-this.ts
+++ src/linter/rules/parser/illegal-super-this.ts
@@ -7,25 +7,56 @@ export const illegalSuperThis: LinterRule = {
   description: 'Report this and super usage outside of a class',
   level: Level.ERROR,
   create(walker, report) {
-    function inClass(parents: grammar.Node[]): boolean {
-      return parents.some(parent => parent.type === grammar.SyntacticToken.CLASS_DECL);
+    function getClassProp(parents: grammar.Node[]): grammar.ClassProp | undefined {
+      for (let i = parents.length - 1; i >= 0; i -= 1) {
+        if (parents[i].type === grammar.SyntacticToken.CLASS_PROP) {
+          return parents[i] as grammar.ClassProp;
+        }
+      }
+
+      return undefined;
     }
 
     walker.onEnter(grammar.Super, (node, ctx) => {
-      if (!inClass(ctx.parents)) {
-        report('You can only use super inside a class', node.span);
+      const classProp = getClassProp(ctx.parents);
+
+      if (!classProp) {
+        report("You can only use 'super' inside a class", node.span);
+        return;
+      }
+
+      if (classProp.static) {
+        report("You can not use 'super' in a static variable or function", node.span);
       }
     });
 
     walker.onEnter(grammar.MemberExpression, (node, ctx) => {
-      if (node.property.type === grammar.LexicalToken.SUPER && !inClass(ctx.parents)) {
-        report('You can only use super inside a class', node.span);
+      if (node.property.type !== grammar.LexicalToken.SUPER) {
+        return;
+      }
+
+      const classProp = getClassProp(ctx.parents);
+
+      if (!classProp) {
+        report("You can only use 'super' inside a class", node.property.span);
+        return;
+      }
+
+      if (classProp.static) {
+        report("You can not use 'super' in a static variable or function", node.property.span);
       }
     });
 
     walker.onEnter(grammar.This, (node, ctx) => {
-      if (!inClass(ctx.parents)) {
-        report('You can only use this inside a class', node.span);
+      const classProp = getClassProp(ctx.parents);
+
+      if (!classProp) {
+        report("You can only use 'this' inside a class", node.span);
+        return;
+      }
+
+      if (classProp.static) {
+        report("You can not use 'this' in a static variable or function", node.span);
       }
     });
   },
--- tests/linter/parser/illegal-super-this.test.ts
+++ tests/linter/parser/illegal-super-this.test.ts
@@ -2,8 +2,11 @@ import { testRule } from '../rule-tester';
 
 import * as parser from '../../../src/linter/rules/parser';
 
-const SUPER_ERROR = 'You can only use super inside a class';
-const THIS_ERROR = 'You can only use this inside a class';
+const SUPER_ERROR = "You can only use 'super' inside a class";
+const STATIC_SUPER_ERROR = "You can not use 'super' in a static variable or function";
+
+const THIS_ERROR = "You can only use 'this' inside a class";
+const STATIC_THIS_ERROR = "You can not use 'this' in a static variable or function";
 
 testRule('illegalSuperThis', {
   rules: parser,
@@ -27,13 +30,22 @@ testRule('illegalSuperThis', {
     { code: 'if x { super }', errors: [SUPER_ERROR] },
     { code: 'function x() { super }', errors: [SUPER_ERROR] },
 
+    { code: 'class C { static var x = super }', errors: [STATIC_SUPER_ERROR] },
+    { code: 'class C { static function x() { super } }', errors: [STATIC_SUPER_ERROR] },
+
     { code: 'C.super', errors: [SUPER_ERROR] },
     { code: 'if x { C.super }', errors: [SUPER_ERROR] },
     { code: 'function x() { C.super }', errors: [SUPER_ERROR] },
+
+    { code: 'class C { static var x = C.super }', errors: [STATIC_SUPER_ERROR] },
+    { code: 'class C { static function x() { C.super } }', errors: [STATIC_SUPER_ERROR] },
 
     // This
     { code: 'this', errors: [THIS_ERROR] },
     { code: 'if x { this }', errors: [THIS_ERROR] },
     { code: 'function x() { this }', errors: [THIS_ERROR] },
+
+    { code: 'class C { static var x = this }', errors: [STATIC_THIS_ERROR] },
+    { code: 'class C { static function x() { this } }', errors: [STATIC_THIS_ERROR] },
   ],
 });
